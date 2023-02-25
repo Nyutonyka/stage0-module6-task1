57af86d1d895c97912cf755400132a8d3ce87431
nyuto <nuytonyka@gmail.com>
2023-02-25 11:42:19 +0200

    Solved

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -2,4 +2,54 @@ package com.epam.mjc.stage0;
 
 public class Animal {
 
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    public Animal() {
+    }
+
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumberOfPaws() {
+        return numberOfPaws;
+    }
+
+    public boolean isHasFur() {
+        return hasFur;
+    }
+
+    public String getNumberOfPaws(int numberOfPaws) {
+        if (numberOfPaws == 1) {
+            return "paw";
+        } else {
+            return "paws";
+        }
+    }
+
+    public String isHasFur(boolean hasFur) {
+        if (hasFur) {
+            return "a";
+        } else {
+            return "no";
+        }
+    }
+
+    public String getDescription(){
+        String color = getColor();
+        String s = isHasFur(isHasFur());
+        int numberOfPaws = getNumberOfPaws();
+        String r = getNumberOfPaws(numberOfPaws);
+        return "This animal is mostly " + color +
+                ". It has "+ numberOfPaws +
+                " " + r +" and " + s + " fur.";
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,19 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal{
 
+    public Bird() {
+        super("blue", 2, false);
+    }
+
+    @Override
+    public String getDescription(){
+        String color = getColor();
+        String s = isHasFur(isHasFur());
+        int numberOfPaws = getNumberOfPaws();
+        String r = getNumberOfPaws(numberOfPaws);
+        return "This animal is mostly " + color +
+                ". It has "+ numberOfPaws +
+                " " + r +" and " + s + " fur. Moreover, it has 2 wings and can fly.";
+    }
 }
